10381b1f7a4ad922298a77af3d17d2770ea4f279
YaSamsonov <7431234@gmail.com>
2015-10-01 14:28:17 +0300

    добавили класс Точка

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,7 +10,18 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Drow();
+
+
+            Point p2 = new Point();
+            p2.x = 5;
+            p2.y = 7;
+            p2.sym = '#';
+            p2.Drow();
             Console.ReadLine();
         }
     }
