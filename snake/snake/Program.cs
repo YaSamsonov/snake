c8dae311e16b5586842c4af9516293eb666e6101
YaSamsonov <7431234@gmail.com>
2015-10-01 14:03:11 +0300

    Добавили процедуру

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,8 +10,18 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            int x1 = 1;
+            int y1 = 3;
+            char sym = '*';
+
+            Draw(x1, y1, sym);
             Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
